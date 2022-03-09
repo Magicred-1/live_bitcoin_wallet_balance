7e7c36a310d48bb7c7ee20219000a729291db509
Djason Gadiou <86433099+Magicred-1@users.noreply.github.com>
2022-03-10 00:05:58 +0100

    Implementing the working API & regex for the BTC Adress
    It almost works properly

--- js/app.js
+++ js/app.js
@@ -1,39 +1,53 @@
 document.addEventListener("DOMContentLoaded", () => {
     console.log("loaded");
-    // Get all the balance elements
+    // Catch the wallet name element and set it to the wallet name
+    let wallet_name = document.getElementById("wallet_name")
+    wallet_name.innerHTML = "Please insert your BTC account adress.";
 
-    let progress = document.getElementById("value"); 
-    // Show the wallet name 
-    let wallet_name = document.getElementById("wallet_name");
+    // Verification of the wallet address using regex
+    let bitcoinAdressRegex = /^[13][a-km-zA-HJ-NP-Z1-9]{25,34}$/;
+    
 
-    // using external API to get the balance of the wallet in satoshis
-    let url = "https://blockchain.info/q/addressbalance/" + wallet_name;
-    let result = fetch(url);
-    result.then(response => response.json())
-    .then(data => {
-        let balance = data;
-        // show the balance in satoshis and convert to BTC
-        let wallet_balance_btc = document.getElementById("wallet_balance_btc").innerHTML = 0. + data + " BTC";
-        // convert satoshis to usd
-        let wallet_balance_usd = document.getElementById("wallet_balance_usd").innerHTML = "$ " + ((0. + data ) * 0.0025);
-    });
-
-    // Get the account typed in the input text
-    let account = document.getElementById("account").value /*= "Hello World"*/;
-
-    /* let wallet_name = "Test Wallet"; */
-    /* alert(value_progressbar); */
-    // Get the balance of the account
-    let balance = document.getElementById("balance");   
-
+    // Change the value of the wallet name
     addEventListener("change", () => {
         let account = document.getElementById("account").value;
-        wallet_name.innerHTML = "Wallet name : " + account;
-    });
+        // First we verify if the account is valid
+            if (account && account.match(bitcoinAdressRegex)) {
 
-    if (!!account) {
-        wallet_name.innerHTML = "Wallet name : " + account;
-    } else {
-        wallet_name.innerHTML = "Please insert your BTC account adress.";
-    }
+                // If the account is valid we set the wallet name
+                wallet_name.innerHTML = "Wallet name : " + account;
+
+                // We insert the wallet both BTC & USD balance into the DOM
+                let infos = document.getElementById('wallet_name');
+                infos.insertAdjacentHTML('beforeend', '<p id="wallet_balance_btc"></p><p id="wallet_balance_usd"></p>');
+
+                // Using external API to get the balance in satoshi selected wallet
+                let url = "https://blockchain.info/q/getreceivedbyaddress/";
+                url += account;
+                // Example : url += "3GKzR29LdyXg8Vao8Mni4MkyrTKXyeDbfN";
+                url += "?confirmations=0";
+
+                // Fetching the balance from the API
+                let result = fetch(url);
+                result.then(response => response.json())
+                .then(data =>   {
+                    // Getting the response in a variable
+                    let balance = data;
+
+                    // Show the balance in satoshis and convert to BTC
+                    let wallet_balance_satoshis 
+                    let wallet_balance_btc =  document.getElementById("wallet_balance_btc").innerHTML = "0." + data + " <em class=\"fa-brands fa-btc\"></em>";
+
+                    // Convert satoshis to usd
+                    // Formula for calculating the price based on the amount of satoshis
+                    // [USD/BTC] * [BTC/SAT] * [SAT/1]
+                    let formula;
+                    let wallet_balance_usd = document.getElementById("wallet_balance_usd").innerHTML = "$ " + ((0. + data ) * 0.0025);
+                });
+            } else {
+                wallet_name.innerHTML = "Please insert a valid BTC account adress.";
+            }
+    });
 });
+
+// made by @djasongadiou
